aeb32cf240c22a7f3008d5defd8fe5ae6af41b82
Arne Klimmt <arne-klimmt@gmx.de>
2025-07-18 15:03:32 +0200

    shortened: evoChain.js

--- scripts/evolutionChain.js
+++ scripts/evolutionChain.js
@@ -9,7 +9,6 @@ async function fetchEvolutionChainData(objectsOfAllPokemonIndex, contentStatus) 
 
 function getNamesOfPokemons(evoObj) {
     evoNamesArray = [];
-
     let firstEvo = evoObj.chain.species.name
     evoNamesArray.push(firstEvo)
 
@@ -24,46 +23,41 @@ function getNamesOfPokemons(evoObj) {
     }
 }
 
-async function changeInnerHTMLEvoChain(objectsOfAllPokemonIndex) { // objectsOfAllPokemonIndex
-    let imgRenderSpot = document.getElementById(`renderCategorieContent${objectsOfAllPokemonIndex}`) // objectsOfAllPokemonIndex
-    imgRenderSpot.innerHTML = "";
-    imgRenderSpot.classList.add('evolutionImgs')
+async function changeInnerHTMLEvoChain(objectsOfAllPokemonIndex) {
+    setupEvoChainRenderSpot(objectsOfAllPokemonIndex)
     let pokeObject = objectsOfAllPokemon[objectsOfAllPokemonIndex]
     let array;
-    let fetchedPokeObj;
 
     for (let indexOfEvoNames = 0; indexOfEvoNames < evoNamesArray.length; indexOfEvoNames++) {
         let pokeEvoName = evoNamesArray[indexOfEvoNames]
 
         if (foundPokemonsArray.length > 1) {
             array = foundPokemonsArray
-            let nameToFetch = checkIfNameExist(pokeEvoName, foundPokemonsArray)
-            if (nameToFetch) {
-                fetchedPokeObj = await fetchSinglePokemon(nameToFetch, foundPokemonsArray)
-            }
+            await checkNameAndFetchData(pokeEvoName, foundPokemonsArray, pokeObject.id)
         } else {
             createManipulatableObject()
-            let nameToFetch = checkIfNameExist(pokeEvoName, objectsOfAllPokemon)
-            if (nameToFetch) {
-                fetchedPokeObj = await fetchSinglePokemon(nameToFetch, objectsOfAllPokemon, pokeObject.id)
-
-            } array = changeAbleObjectOfAllPokemon
-
+            await checkNameAndFetchData(pokeEvoName, objectsOfAllPokemon, pokeObject)
+            array = changeAbleObjectOfAllPokemon
         }
-        loopThroughPokemonsArray(pokeEvoName, imgRenderSpot, array)
-
+        changeInnerHTML(pokeEvoName, array, objectsOfAllPokemonIndex)
     }
 }
 
-function addEvoChainSearchBar() {
-
+function setupEvoChainRenderSpot(objectsOfAllPokemonIndex) {
+    let imgRenderSpot = document.getElementById(`renderCategorieContent${objectsOfAllPokemonIndex}`)
+    imgRenderSpot.innerHTML = "";
+    imgRenderSpot.classList.add('evolutionImgs')
 }
 
-function addEvoChainNormal() {
-
+async function checkNameAndFetchData(pokeEvoName, objectsOfAllPokemon, pokeObject) {
+    let nameToFetch = checkIfNameExist(pokeEvoName, objectsOfAllPokemon)
+    if (nameToFetch) {
+        fetchedPokeObj = await fetchSinglePokemon(nameToFetch, objectsOfAllPokemon, pokeObject.id)
+    }
 }
 
-function loopThroughPokemonsArray(pokeEvoName, imgRenderSpot, array) {
+function changeInnerHTML(pokeEvoName, array, objectsOfAllPokemonIndex) {
+    let imgRenderSpot = document.getElementById(`renderCategorieContent${objectsOfAllPokemonIndex}`)
     for (let index = 0; index < array.length; index++) {
         if (pokeEvoName == array[index].name) {
             let src = srcRequest(array[index])
@@ -90,7 +84,6 @@ function srcRequest(obj) {
 }
 
 async function fetchSinglePokemon(pokeName, array, pokeID) {
-
     try {
         const singlePokemonURL = `https://pokeapi.co/api/v2/pokemon/${pokeName}`
         let singlePokeResponse = await fetch(singlePokemonURL);
@@ -103,9 +96,7 @@ async function fetchSinglePokemon(pokeName, array, pokeID) {
         if (array.length < 20) {
             foundPokemonsArray.push(singlePokeResponseToJSON)
         }
-    } catch (error) {
-
-    }
+    } catch (error) { }
 }
 
 function createManipulatableObject() {
